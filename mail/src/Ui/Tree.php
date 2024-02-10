7ef00b4d7eea44d2445198c80fd5af9b6c007809
Alexandros Sigalas <asig@sigalas.eu>
2024-02-10 17:05:46 +0200

    PATCH CORE: Allow hiding mail accounts from the mail accounts tree of the mail app by placing a dot(.) at the beginning of the account name.
    (cherry picked from commit 8b6e0147b7b99906528113a9a395bc8c42192f88)

--- mail/src/Ui/Tree.php
+++ mail/src/Ui/Tree.php
@@ -108,7 +108,7 @@ class Tree
 	{
 		$roots = array(TreeWidget::ID => 0, TreeWidget::CHILDREN => array());
 
-		foreach(Mail\Account::search(true, 'params') as $acc_id => $params)
+		foreach(Mail\Account::search(true, 'params', null, false, 0, true, true) as $acc_id => $params)
 		{
 			// checking a single requested account only: skip everything else before it can
 			// trigger a live is_imap() connection attempt for accounts we don't even want

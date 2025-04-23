a50b94567135c1ec82164da5e1fcd8b06bf7dfb1
Alexandros Sigalas <asig@sigalas.eu>
2025-04-23 08:19:00 +0300

    PATCH CORE: Nextmatch - updateColumns try with applyFilters

--- api/js/etemplate/et2_extension_nextmatch.ts
+++ api/js/etemplate/et2_extension_nextmatch.ts
@@ -2176,6 +2176,7 @@ export class et2_nextmatch extends et2_DOMWidget implements et2_IResizeable, et2
 				self.activeFilters.selectcols.push('lettersearch');
 			}
 			//self.getInstanceManager().submit();
+			self.applyFilters();
 
 			self.selectPopup = null;
 		};
